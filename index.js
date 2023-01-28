6960850924f6d6150b6dfd10c367c2a00efeae9f
Andres Lozada <andres.lozada@uao.edu.co>
2023-01-28 11:42:31 -0500

    Ejemplo de las funciones de los array 'sin explicacion'

--- index.js
+++ index.js
@@ -1,22 +1,181 @@
-const array1 = [5, 12, 8, 130, 44];
+// 1. Concat():
 
-console.log(`El array es ${array1}`);
+var arr1 = ["a", "b", "c"];
+var arr2 = ["d", "e", "f"];
+var arr3 = arr1.concat(arr2);
 
-let index = 2;
+console.log(arr3); // Output: ["a", "b", "c", "d", "e", "f"]
 
-console.log(`Using an index of ${index} the item returned is ${array1.at(index)}`);
-// Expected output: "Using an index of 2 the item returned is 8"
+// 2. Every():
 
-index = -1;
+var numbers = [1, 2, 3, 4, 5];
 
-console.log(`Using an index of ${index} item returned is ${array1.at(index)}`);
-// Expected output: "Using an index of -2 item returned is 130"
+var everyResult = numbers.every(function(value, index, array) {
+  return value < 10;
+});
 
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2).concat(array1);
+console.log(everyResult); // Output: true
 
-array3.fill("holas", 5, 10)
+// 3. Fill():
 
-array3[56] = "mundo"
+var numbers = [1, 2, 3, 4, 5];
 
-console.log(array3);
+var fillResult = numbers.fill(1, 2, 4);
+
+console.log(fillResult); // Output: [1, 2, 1, 1, 5]
+
+// 4. Filter():
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var filterResult = numbers.filter(function(value, index, array) {
+  return value > 5;
+});
+
+console.log(filterResult); // Output: [6, 7, 8, 9, 10]
+
+// 5. Find():
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var findResult = numbers.find(function(value, index, array) {
+  return value > 5;
+});
+
+console.log(findResult); // Output: 6
+
+// 6. FindIndex():
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+var findIndexResult = numbers.findIndex(function(value, index, array) {
+  return value > 5;
+});
+
+console.log(findIndexResult); // Output: 5
+
+// 7. ForEach():
+
+var numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach(function(value, index, array) {
+  console.log(value);
+});
+
+// Output: 
+// 1
+// 2
+// 3
+// 4
+// 5
+
+// 8. IndexOf():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var indexOfResult = numbers.indexOf(3);
+
+console.log(indexOfResult); // Output: 2
+
+// 9. Join():
+
+var words = ["Hello", "World"];
+
+var joinResult = words.join(" ");
+
+console.log(joinResult); // Output: "Hello World"
+
+// 10. LastIndexOf():
+
+var numbers = [1, 2, 3, 1, 2, 3, 4, 5];
+
+var lastIndexOfResult = numbers.lastIndexOf(3);
+
+console.log(lastIndexOfResult); // Output: 5
+
+// 11. Map():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var mapResult = numbers.map(function(value, index, array) {
+  return value * 2;
+});
+
+console.log(mapResult); // Output: [2, 4, 6, 8, 10]
+
+// 12. Pop():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var popResult = numbers.pop();
+
+console.log(popResult); // Output: 5
+
+// 13. Push():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var pushResult = numbers.push(6);
+
+console.log(pushResult); // Output: 6
+
+// 14. Reduce():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var reduceResult = numbers.reduce(function(previousValue, currentValue, currentIndex, array) {
+  return previousValue + currentValue;
+});
+
+console.log(reduceResult); // Output: 15
+
+// 15. ReduceRight():
+
+var numbers = [1, 2, 3, 4, 5];
+
+var reduceRightResult = numbers.reduceRight(function(previousValue, currentValue, currentIndex, array) {
+  return previousValue + currentValue;
+});
+
+console.log(reduceRightResult); // Output: 15
+
+// 16. reverse(): 
+let array = [1, 2, 3, 4, 5];
+console.log(array.reverse()); // [5, 4, 3, 2, 1]
+
+// 17. shift(): 
+array = [1, 2, 3, 4, 5];
+console.log("Metodo shift");
+console.log(array.shift()); // 1
+console.log(array); // [2, 3, 4, 5]
+
+// 18. slice(): 
+array = [1, 2, 3, 4, 5];
+console.log(array.slice(2, 4)); // [3, 4]
+
+// 19. some(): 
+array = [1, 2, 3, 4, 5];
+let isGreaterThanFour = (element) => element > 4;
+console.log(array.some(isGreaterThanFour)); // true
+
+// 20. sort(): 
+array = [3, 5, 1, 4, 2];
+console.log(array.sort()); // [1, 2, 3, 4, 5]
+
+// 21. splice(): 
+array = [1, 2, 3, 4, 5];
+console.log(array.splice(2, 2)); // [3, 4]
+console.log(array); // [1, 2, 5]
+
+// 22. toString(): 
+array = [1, 2, 3, 4, 5];
+console.log(array.toString()); // "1,2,3,4,5"
+
+// 23. unshift(): 
+array = [1, 2, 3, 4, 5];
+console.log(array.unshift(6)); // 6
+console.log(array); // [6, 1, 2, 3, 4, 5]
+
+// 24. valueOf(): 
+array = [1, 2, 3, 4, 5];
+console.log(array.valueOf()); // [1, 2, 3, 4, 5]
